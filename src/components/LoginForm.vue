7d68241ae1ded399f07d4008abb5e04237a488a3
Ali Sadeghi <alisadghi228@gmail.com>
2023-12-11 21:07:29 +0330

    add project to git

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="card-front">
+    <div class="center-wrap">
+      <Form
+        @submit="login"
+        :validation-schema="schema"
+        class="section text-center"
+      >
+        <h4 class="mb-4 pb-3">ورود</h4>
+        <div class="form-group">
+          <Field
+            name="email"
+            type="email"
+            class="form-style"
+            placeholder="ایمیل خود را وارد کنید"
+            autocomplete="off"
+          />
+          <i class="input-icon uil uil-at"></i>
+        </div>
+        <ErrorMessage name="email" />
+        <div class="form-group mt-2">
+          <Field
+            name="password"
+            type="password"
+            class="form-style"
+            placeholder="پسورد را وارد کنید"
+            autocomplete="off"
+          />
+          <i class="input-icon uil uil-lock-alt"></i>
+        </div>
+        <ErrorMessage name="password" />
+
+        <button type="submit" class="btn mt-4">ورود به سایت</button>
+        <p class="mb-0 mt-4 text-center">
+          <a class="link"> آیا کلمه عبور خود را فراموش کرده اید ؟ </a>
+        </p>
+      </Form>
+    </div>
+  </div>
+</template>
+
+<script>
+import { Form, Field, ErrorMessage } from "vee-validate";
+import * as Yup from "yup";
+
+export default {
+  name: "LoginForm",
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
+  data() {
+    const schema = Yup.object({
+      email: Yup.string()
+        .required("ایمیل را وارد کنید")
+        .email("ایمیل نامعتبر است."),
+      password: Yup.string()
+        .required("وارد کردن پسورد الزامی است.")
+        .min(4, "پسورد باید حداقل 4 کاراکتر باشد.")
+        .max(20, "پسورد نمی تواند بیشتر از 20 کاراکتر باشد."),
+    });
+    return {
+      schema,
+    };
+  },
+  methods: {
+    login(values) {
+      // console.log(`email : ${this.email} , password : ${this.email}`);
+      console.log(values);
+    },
+  },
+};
+</script>
---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,132 @@
+<template>
+  <div class="card-back">
+    <div class="center-wrap">
+      <form @submit.prevent="register" class="section text-center">
+        <h4 class="mb-4 pb-3">ثبت نام</h4>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <input
+                type="text"
+                name="logname"
+                class="form-style"
+                placeholder="نام و نام خانوادگی"
+                id="logname"
+                autocomplete="off"
+                v-model="name"
+              />
+              <i class="input-icon uil uil-user"></i>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group">
+              <input
+                type="email"
+                class="form-style"
+                placeholder="ایمیل خود را وارد کنید"
+                autocomplete="off"
+                v-model="email"
+              />
+              <i class="input-icon uil uil-at"></i>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group mt-2">
+              <input
+                type="password"
+                name="password"
+                class="form-style"
+                placeholder="کلمه عبور"
+                id="password"
+                autocomplete="off"
+                v-model="password"
+              />
+              <i class="input-icon uil uil-lock-alt"></i>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="form-group mt-2">
+              <input
+                type="password"
+                class="form-style"
+                placeholder="تکرار کلمه عبور"
+                autocomplete="off"
+                v-model="confirmPassword"
+              />
+              <i class="input-icon uil uil-lock-alt"></i>
+            </div>
+          </div>
+          <div class="col-md-6 mt-2">
+            <div class="form-group mt-2">
+              <input
+                type="number"
+                class="form-style"
+                placeholder="شماره تلفن"
+                autocomplete="off"
+                v-model="phoneNumber"
+              />
+              <i class="input-icon uil uil-phone"></i>
+            </div>
+          </div>
+          <div class="col-md-6 mt-4 text-center">
+            <div class="form-group">
+              <label for="1">نامشخص</label>
+              <input
+                class="m-2"
+                type="radio"
+                id="1"
+                value="نامشخص"
+                name="gender"
+                v-model="gender"
+              />
+
+              <label for="2">آقا</label>
+              <input
+                class="m-2"
+                type="radio"
+                id="2"
+                value="آقا"
+                name="gender"
+                v-model="gender"
+              />
+
+              <label for="3">خانم</label>
+              <input
+                class="m-2"
+                type="radio"
+                id="3"
+                value="خانم"
+                name="gender"
+                v-model="gender"
+              />
+            </div>
+          </div>
+        </div>
+        <button class="btn mt-4" type="submit">ثبت نام</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "RegisterForm",
+  data() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      confirmPassword: "",
+      gender: "نامشخص",
+      phoneNumber: "",
+    };
+  },
+  methods: {
+    register() {
+      console.log(
+        `name : ${this.name} , email : ${this.email} ,password : ${this.password} , confirmPassword : ${this.confirmPassword} , phoneNumber : ${this.phoneNumber} , gender : ${this.gender}`
+      );
+    },
+  },
+};
+</script>
