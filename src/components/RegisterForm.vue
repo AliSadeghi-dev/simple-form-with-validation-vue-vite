4871a40f65efd7dc7bb682189394534c1733ff39
Ali Sadeghi <alisadghi228@gmail.com>
2023-12-11 21:26:31 +0330

    feat : register page validation added

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -1,72 +1,82 @@
 <template>
   <div class="card-back">
     <div class="center-wrap">
-      <form @submit.prevent="register" class="section text-center">
+      <Form
+        @submit="register"
+        class="section text-center"
+        :validation-schema="schema"
+      >
         <h4 class="mb-4 pb-3">ثبت نام</h4>
         <div class="row">
           <div class="col-md-6">
             <div class="form-group">
-              <input
+              <Field
+                name="name"
                 type="text"
-                name="logname"
                 class="form-style"
                 placeholder="نام و نام خانوادگی"
                 id="logname"
                 autocomplete="off"
-                v-model="name"
+                validate-on-input="true"
               />
               <i class="input-icon uil uil-user"></i>
             </div>
+            <ErrorMessage name="name" />
           </div>
           <div class="col-md-6">
             <div class="form-group">
-              <input
-                type="email"
+              <Field
+                name="email"
                 class="form-style"
                 placeholder="ایمیل خود را وارد کنید"
                 autocomplete="off"
-                v-model="email"
+                validate-on-input="true"
               />
               <i class="input-icon uil uil-at"></i>
             </div>
+            <ErrorMessage name="email" />
           </div>
           <div class="col-md-6">
             <div class="form-group mt-2">
-              <input
+              <Field
                 type="password"
                 name="password"
                 class="form-style"
                 placeholder="کلمه عبور"
-                id="password"
                 autocomplete="off"
-                v-model="password"
+                validate-on-input="true"
               />
               <i class="input-icon uil uil-lock-alt"></i>
             </div>
+            <ErrorMessage name="password" />
           </div>
           <div class="col-md-6">
             <div class="form-group mt-2">
-              <input
+              <Field
                 type="password"
+                name="confirmPassword"
                 class="form-style"
                 placeholder="تکرار کلمه عبور"
                 autocomplete="off"
-                v-model="confirmPassword"
+                validate-on-input="true"
               />
               <i class="input-icon uil uil-lock-alt"></i>
             </div>
+            <ErrorMessage name="confirmPassword" />
           </div>
           <div class="col-md-6 mt-2">
             <div class="form-group mt-2">
-              <input
+              <Field
                 type="number"
                 class="form-style"
                 placeholder="شماره تلفن"
                 autocomplete="off"
-                v-model="phoneNumber"
+                name="phoneNumber"
+                validate-on-input="true"
               />
               <i class="input-icon uil uil-phone"></i>
             </div>
+            <ErrorMessage name="phoneNumber" />
           </div>
           <div class="col-md-6 mt-4 text-center">
             <div class="form-group">
@@ -103,22 +113,40 @@
           </div>
         </div>
         <button class="btn mt-4" type="submit">ثبت نام</button>
-      </form>
+      </Form>
     </div>
   </div>
 </template>
 
 <script>
+import { Form, Field, ErrorMessage } from "vee-validate";
+import * as Yup from "yup";
 export default {
   name: "RegisterForm",
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
   data() {
+    const schema = Yup.object({
+      name: Yup.string().required("نام و نام خانوادگی را وارد کنید."),
+      email: Yup.string()
+        .email("ایمیل وارد شده معتبر نمی باشد.")
+        .required("لطفا ایمیل را وارد کنید."),
+      password: Yup.string()
+        .required("وارد کردن پسورد الزامی است.")
+        .min(4, "پسورد باید حداقل 4 کاراکتر باشد.")
+        .max(20, "پسورد نمی تواند بیشتر از 20 کاراکتر باشد."),
+      confirmPassword: Yup.string()
+        .oneOf([Yup.ref("password"), null], "تایید پسورد صحیح نمی‌باشد")
+        .required("تایید پسورد الزامی است"),
+      phoneNumber: Yup.string()
+        .required("لطفا شماره موبایل را وارد کنید.")
+        .length(11, "شماره تلفن نامعتبر است."),
+    });
     return {
-      name: "",
-      email: "",
-      password: "",
-      confirmPassword: "",
-      gender: "نامشخص",
-      phoneNumber: "",
+      schema,
     };
   },
   methods: {
